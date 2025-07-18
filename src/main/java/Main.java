239e0ffb656c234a09e1cebdd4052a0c7d8113a8
Kunals990 <kunalsable9990@gmail.com>
2025-07-18 23:42:19 +0530

    Add ping command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,6 +18,15 @@ public class Main {
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
+          InputStream in = clientSocket.getInputStream();
+          BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+          OutputStream outputStream = clientSocket.getOutputStream();
+          String line = reader.readLine();
+          if(line.equals("PING")){
+              outputStream.write("+PONG\r\n".getBytes());
+          }
+
+
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
