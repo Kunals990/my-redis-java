1277c79ea4d85e59c9ecdcfaa647acb9bb3b89f2
Kunals990 <kunalsable9990@gmail.com>
2025-07-23 21:39:42 +0530

    Add multiple concurrant clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,58 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+
+class ClientHandler extends Thread {
+    public final Socket clientsocket;
+
+    ClientHandler(Socket clientsocket) {
+        this.clientsocket = clientsocket;
+    }
+
+    public void run () {
+        try(InputStream inputStream = clientsocket.getInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+             OutputStream outputStream = clientsocket.getOutputStream()
+        ){
+            String inputLine;
+            while((inputLine=reader.readLine())!=null){
+                if ("PING".equals(inputLine)) {
+                    outputStream.write("+PONG\r\n".getBytes());
+                    outputStream.flush();
+                }
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }finally {
+            try{
+                clientsocket.close();
+            }catch (IOException e){};
+        }
+    }
+}
 
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-        int port = 6379;
-        try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-          while(!clientSocket.isClosed()){
-              InputStream in = clientSocket.getInputStream();
-              BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-              OutputStream outputStream = clientSocket.getOutputStream();
-              String line1 = reader.readLine();
-              String line2 = reader.readLine();
-              String line3 = reader.readLine();
-              if("PING".equals(line3)){
-                  outputStream.write("+PONG\r\n".getBytes());
-                  outputStream.flush();
-              }
-          }
-            clientSocket.close();
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-          }
-        }
+
+    int port = 6379;
+    try (ServerSocket serverSocket = new ServerSocket(port)){
+      serverSocket.setReuseAddress(true);
+
+      while(true){
+          Socket clientSocket = serverSocket.accept();
+
+          ClientHandler handler = new ClientHandler(clientSocket);
+          handler.start();
+      }
+
+
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
   }
 }
