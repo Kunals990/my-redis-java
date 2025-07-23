98ffbf2e52156bdd02aaf87de84dfedd3924abc3
Kunals990 <kunalsable9990@gmail.com>
2025-07-23 21:43:59 +0530

    Add multiple concurrant clients-1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,6 +34,7 @@ class ClientHandler extends Thread {
 }
 
 public class Main {
+
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
