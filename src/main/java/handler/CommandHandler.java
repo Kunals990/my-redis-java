96726b476cd001aae56fd0d89ba04d6ddd8cb743
Kunals990 <kunalsable9990@gmail.com>
2025-07-26 11:51:08 +0530

    Added RPUSH Command-fixed

--- src/main/java/handler/CommandHandler.java
+++ src/main/java/handler/CommandHandler.java
@@ -14,7 +14,7 @@ public class CommandHandler {
         commandMap.put("ECHO",new EchoCommand());
         commandMap.put("SET",new SetCommand());
         commandMap.put("GET",new GetCommand());
-        commandMap.put("RpushCommand",new RpushCommand());
+        commandMap.put("RPUSH",new RpushCommand());
     }
 
     public static String handle(List<String> args) {
