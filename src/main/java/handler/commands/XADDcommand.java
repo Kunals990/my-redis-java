3c01c53313e23bee8bcd2e12e3aa3639fba3f788
Kunals990 <kunalsable9990@gmail.com>
2025-07-28 22:39:42 +0530

    Adding Blocking Reads-fixed- Added 0-* support-fixed issue-8

--- src/main/java/handler/commands/XADDcommand.java
+++ src/main/java/handler/commands/XADDcommand.java
@@ -96,7 +96,7 @@ public class XADDcommand implements Command {
                         maxSeq = Math.max(maxSeq, seq);
                     }
                 }
-                seqNum = (maxSeq == -1) ? 1 : maxSeq + 1;
+                seqNum = (maxSeq == -1) ? 0 : maxSeq + 1;
 
             } else {
                 try {
