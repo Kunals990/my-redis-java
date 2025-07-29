d47e149f8a14c6a1c797c30c7a33d16bca117157
Kunals990 <kunalsable9990@gmail.com>
2025-07-29 23:36:04 +0530

    Recieve Handshake -2 - PSYNC Command -fixed

--- src/main/java/handler/commands/PSYNCcommand.java
+++ src/main/java/handler/commands/PSYNCcommand.java
@@ -12,6 +12,6 @@ public class PSYNCcommand implements Command {
     @Override
     public String execute(List<String> args, SocketChannel clientChannel) throws IOException {
         String replicationId = ServerConfig.getMaster_replid();
-        return "+FULLRESYNC"+replicationId+" 0\r\n";
+        return "+FULLRESYNC "+replicationId+" 0\r\n";
     }
 }
