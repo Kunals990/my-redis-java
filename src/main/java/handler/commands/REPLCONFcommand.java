826620392a4fa4bde532f012de4fb654236d181e
Kunals990 <kunalsable9990@gmail.com>
2025-07-30 13:04:35 +0530

    Write message - Replica Propagation-fixed REPLCONF command

--- src/main/java/handler/commands/REPLCONFcommand.java
+++ src/main/java/handler/commands/REPLCONFcommand.java
@@ -10,14 +10,17 @@ import java.util.List;
 public class REPLCONFcommand implements Command {
     @Override
     public String execute(List<String> args, SocketChannel clientChannel) throws IOException {
-        if (args.size() == 3 && args.get(1).equalsIgnoreCase("listening-port")) {
+        if (args.get(1).equalsIgnoreCase("capa")) {
+            return "+OK\r\n";
+        }
 
+        if(args.size()==3 && args.get(1).equalsIgnoreCase("listening-port")){
             int port = Integer.parseInt(args.get(2));
 
             ReplicaManager.addReplica(clientChannel, port);
-
             return "+OK\r\n";
         }
         return "-ERR Incorrect number of arguments for REPLCONF\r\n";
+
     }
 }
