57da54139b4303a50dc9225cbb01ae95a294e10b
Kunals990 <kunalsable9990@gmail.com>
2025-07-31 20:03:58 +0530

    Optimized code + Added WAIT command-fixed-debug-true

--- src/main/java/handler/commands/GetCommand.java
+++ src/main/java/handler/commands/GetCommand.java
@@ -11,10 +11,10 @@ public class GetCommand implements Command {
     @Override
     public String execute(List<String> args, SocketChannel clientChannel) {
         if (args.size() < 2) return "-ERR wrong number of arguments for 'get'\r\n";
-
+        System.out.println("In get");
         String key=args.get(1);
         String value = KeyValueStore.getInstance().get(key);
-
+        System.out.println("in get :val "+value);
         if (value == null) return "$-1\r\n";
 
         return "$" + value.length() + "\r\n" + value + "\r\n";
