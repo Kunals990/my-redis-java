1ae05653cc9f2fb5f2589813adb38f38eb246419
Kunals990 <kunalsable9990@gmail.com>
2025-07-31 20:17:52 +0530

    Optimized code + Added WAIT command-fixed-debug-true

--- src/main/java/handler/commands/GetCommand.java
+++ src/main/java/handler/commands/GetCommand.java
@@ -1,5 +1,6 @@
 package handler.commands;
 
+import config.ServerConfig;
 import handler.Command;
 import store.KeyValueStore;
 
@@ -12,6 +13,17 @@ public class GetCommand implements Command {
     public String execute(List<String> args, SocketChannel clientChannel) {
         if (args.size() < 2) return "-ERR wrong number of arguments for 'get'\r\n";
         System.out.println("In get args are "+args);
+
+        if (!ServerConfig.isMaster()) {
+            try {
+                // 50ms is usually more than enough time for the other thread to catch up.
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                // Restore the interrupted status
+                Thread.currentThread().interrupt();
+            }
+        }
+
         String key=args.get(1);
         System.out.println("key is "+key);
         String value = KeyValueStore.getInstance().get(key);
